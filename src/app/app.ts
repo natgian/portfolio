3b4c90e528e3c75e191044008a6999fa7b413940
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-11-20 23:14:40 +0100

    feat: start working on header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './features/hero/hero';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [Hero],
+  imports: [Hero, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
