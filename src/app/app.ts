a1e1dab386f17916447586e2383a532d01a7f368
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-11-24 21:20:30 +0100

    feat: add about section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Hero } from './features/hero/hero';
 import { Header } from './layout/header/header';
+import { Hero } from './features/hero/hero';
+import { About } from './features/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Hero, Header],
+  imports: [Header, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
