e8b5b39d7c326368b75af5855d052af7e18a56c6
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-12-10 21:54:38 +0100

    feat: add validations and input feedback to contact form

--- src/app/features/contact/contact-form/contact-form.ts
+++ src/app/features/contact/contact-form/contact-form.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Button } from '../../../shared/components/button/button';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -13,16 +13,40 @@ export class ContactForm {
   isDarkBackground = true;
 
   contactForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    message: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(100),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}'),
+      Validators.maxLength(254),
+    ]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(2000),
+    ]),
   });
 
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
   handleSubmit() {
-    console.log({
-      name: this.contactForm.value.name,
-      email: this.contactForm.value.email,
-      message: this.contactForm.value.message,
-    });
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
   }
 }
