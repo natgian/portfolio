0b040425c2217c29c9b7e9399197059e60a380e8
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-12-19 16:13:03 +0100

    feat: add router link to footer for legal page

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AnimatedIconLink } from '../../shared/components/animated-icon-link/animated-icon-link';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [AnimatedIconLink],
+  imports: [AnimatedIconLink, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
