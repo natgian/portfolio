97f7650cd291be89ced9b260b2de595d1b80d269
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-12-22 21:58:11 +0100

    feat: add project-detail component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
 import { Home } from './pages/home/home';
+import { ProjectDetail } from './pages/project-detail/project-detail';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'legal-notice',
     title: 'Legal Notice',
     component: LegalNotice,
+  },
+  {
+    path: 'projects/project-detail',
+    title: 'Project',
+    component: ProjectDetail,
   },
 ];
---
+++ src/app/pages/project-detail/project-detail.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Header } from '../../layout/header/header';
+import { StackIcon } from '../../shared/components/stack-icon/stack-icon';
+import { Button } from '../../shared/components/button/button';
+
+@Component({
+  selector: 'app-project-detail',
+  imports: [Header, StackIcon, Button],
+  templateUrl: './project-detail.html',
+  styleUrl: './project-detail.css',
+})
+export class ProjectDetail {
+  isDarkBackground = true;
+  projects = [
+    {
+      name: 'Join',
+      description:
+        'JOIN is a web application developed as part of a Scrum-based group project to gain practical experience in agile teamwork and project management. It is a task manager inspired by the Kanban System that features tasks, subtasks, user assignment and due dates.',
+      details: [
+        'My main responsibilities included implementing the Kanban board feature, building the core UI structure with page navigation, and developing the login and sign-up client-side validation and user feedback.',
+        'We organized our workflow through clear task distribution and regular coordination. This allowed me to work independently on assigned features while ensuring seamless integration into the overall application concept.',
+      ],
+      stack: [
+        { label: 'HTML', icon: '/img/icons/html.svg' },
+        { label: 'CSS', icon: '/img/icons/css.svg ' },
+        { label: 'JavaScript', icon: '/img/icons/javascript.svg ' },
+        { label: 'Firebase', icon: '/img/icons/firebase.svg ' },
+      ],
+      img: '/img/projects/join/join_mockup_2.png',
+      githubLink: 'https://github.com/natgian/kanban-vanillajs-group-project',
+      liveLink: 'https://join.natgian.com/',
+    },
+  ];
+}
