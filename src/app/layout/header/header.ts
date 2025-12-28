ee232d563e1160d1ddc6f2289203271cf7fde04b
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-12-28 11:23:11 +0100

    feat: add dynamic nav-link color change on header based on current route

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
+  readonly router = inject(Router);
+  private sub?: Subscription;
+  isProjectPage = false;
   activeLanguage: 'DE' | 'EN' = 'DE';
+
+  ngOnInit() {
+    this.isProjectPage = this.router.url.startsWith('/projects');
+
+    this.sub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isProjectPage = event.urlAfterRedirects.startsWith('/projects');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
 
   setLanguage(lang: 'DE' | 'EN') {
     this.activeLanguage = lang;
