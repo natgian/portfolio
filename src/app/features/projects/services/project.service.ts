03a2c633550129794230e2edb2adbcd998c2b067
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-12-28 19:00:42 +0100

    feat: add data to project service

--- src/app/features/projects/services/project.service.ts
+++ src/app/features/projects/services/project.service.ts
@@ -38,7 +38,7 @@ export class ProjectService {
         { label: 'JavaScript', icon: '/img/icons/javascript.svg ' },
         { label: 'Firebase', icon: '/img/icons/firebase.svg ' },
       ],
-      detailImg: '/img/projects/join/join_mockup_2.png',
+      detailImg: '/img/projects/join/join_mockup_devices.png',
       githubLink: 'https://github.com/natgian/kanban-vanillajs-group-project',
       liveLink: 'https://join.natgian.com/',
     },
@@ -49,14 +49,18 @@ export class ProjectService {
       cardDescription:
         'Browser-based game built with Vanilla JavaScript to practice object-oriented programming (OOP). Protect the Greenwood with your bow and courage from the creeping darkness rising.',
       isFeatured: false,
-      detailDescription: '',
-      implementationText: [''],
+      detailDescription:
+        'This game was created to practice OOP (object oriented programming). The goal was to provide a fun and interactive experience directly in the browser, without any external frameworks.',
+      implementationText: [
+        'For this project, I structured and implemented the game’s core mechanics, including character movement, jumping, collision detection with enemies and collectibles, and a crafting system for creating new arrows. I added dynamic multi-layered backgrounds, integrated sound effects and background music with mute functionality, and added restart and menu navigation features.',
+        'The project allowed me to strengthen my skills in object-oriented programming, interactive UI design and game logic implementation.',
+      ],
       stack: [
         { label: 'HTML', icon: '/img/icons/html.svg' },
         { label: 'CSS', icon: '/img/icons/css.svg ' },
         { label: 'JavaScript', icon: '/img/icons/javascript.svg ' },
       ],
-      detailImg: '/img/projects/join/join_mockup_2.png',
+      detailImg: '/img/projects/nara/nara_mockup_devices.png',
       githubLink: 'https://github.com/natgian/2dgame-js',
       liveLink: 'https://nara-game.natgian.com/',
     },
@@ -67,8 +71,12 @@ export class ProjectService {
       cardDescription:
         'A full-stack web app for book enthusiasts to manage and organize their reading journey. Built with React, Node.js, Express.js and MongoDB.',
       isFeatured: false,
-      detailDescription: '',
-      implementationText: [''],
+      detailDescription:
+        'A user-friendly Book Tracking App designed for book enthusiasts to manage and organize their reading journey.',
+      implementationText: [
+        'During this project, I independently planned and developed a full-stack web application for searching books, managing reading lists, and tracking reading progress. I designed the user flow and UI structure, implemented core features using React, Node.js, Express, and MongoDB, and integrated the Google Books API. I also built secure account features such as password reset and form validation, and ensured responsive behavior across devices.',
+        'The project strengthened my skills in full-stack development, API integration, and creating user-focused applications from concept to deployment.',
+      ],
       stack: [
         { label: 'HTML', icon: '/img/icons/html.svg' },
         { label: 'CSS', icon: '/img/icons/css.svg ' },
@@ -77,7 +85,7 @@ export class ProjectService {
         { label: 'Node.js', icon: '/img/icons/nodejs.svg ' },
         { label: 'MongoDB', icon: '/img/icons/mongodb.svg ' },
       ],
-      detailImg: '/img/projects/join/join_mockup_2.png',
+      detailImg: '/img/projects/leseoase/leseoase_mockup_devices.png',
       githubLink: 'https://github.com/natgian/books-tracking-app',
       liveLink: 'https://www.leseoase.app/',
     },
