7d13c875cf0e82198873bba1bfb60668be18e014
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-01-16 19:51:36 +0100

    feat: improve responsiveness on all sections and pages

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Header, Footer, TranslatePipe],
+  imports: [Header, Footer, TranslatePipe, RouterLink],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.css',
 })
