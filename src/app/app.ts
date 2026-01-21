bfa8f75d9eca4bc42e5895ea37dd7db5dabc8b6e
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-01-21 22:00:40 +0100

    refactor: remove language logic from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,4 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  private translate = inject(TranslateService);
-  supportedLangs = ['en', 'de'];
-
-  constructor() {
-    this.translate.addLangs(this.supportedLangs);
-    this.translate.setFallbackLang('en');
-
-    const savedLang = localStorage.getItem('lang');
-    const browserLang = this.translate.getBrowserLang();
-    const lang: string =
-      savedLang || (browserLang && this.supportedLangs.includes(browserLang) ? browserLang : 'en');
-
-    this.translate.use(lang);
-    document.documentElement.lang = lang;
-
-    this.translate.onLangChange.subscribe((event) => {
-      document.documentElement.lang = event.lang;
-    });
-  }
-}
+export class App {}
