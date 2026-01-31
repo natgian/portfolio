fdb0dda4abef1fb6afeae22349fa6ccdde2ebdeb
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-01-31 16:12:09 +0100

    feat: add titleService and translateService to legal page to translate document title tag

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,6 @@ export const routes: Routes = [
   },
   {
     path: 'legal-notice',
-    title: 'Legal Notice',
     component: LegalNotice,
   },
   {
--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-legal-notice',
@@ -14,4 +16,39 @@ import { RouterLink } from '@angular/router';
 /**
  * Page component responsible for rendering the legal page.
  */
-export class LegalNotice {}
+export class LegalNotice {
+  private sub: Subscription;
+
+  /**
+   * Subscribes to TranslationService to detect language changes and set the title tag accordingly.
+   *
+   * @param titleService - Angular Title service used to set the document title
+   * @param translate - ngx-translate service used to fetch the translations
+   */
+  constructor(
+    private titleService: Title,
+    private translate: TranslateService,
+  ) {
+    this.setTitle();
+
+    this.sub = this.translate.onLangChange.subscribe(() => {
+      this.setTitle();
+    });
+  }
+
+  /**
+   * Sets the translated title in the document title tag based on the current language.
+   */
+  private setTitle() {
+    this.translate.get('legal.title').subscribe((translatedTitle) => {
+      this.titleService.setTitle(translatedTitle);
+    });
+  }
+
+  /**
+   * Unsubscribes from the language change subscription to prevent memory leaks.
+   */
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+}
